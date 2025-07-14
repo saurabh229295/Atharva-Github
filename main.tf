f34de6e34b2ccd75d0979e66f2737a77959b0d3d
unknown <singh.saurabh2292@gmail.com>
2025-07-14 11:46:11 +0530

    jaydecodeadded

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ resource "azurerm_resource_group" "scare" {
   name     = "Simba101"
   location = "East US"
 }
+resource "azurerm_resource_group" "rgblock" {
+  name     = "Simba505"
+  location = "East US"
+}
